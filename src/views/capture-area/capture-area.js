3222d27ac3a088f2ad35070cbd8d20adf87d418c
Arindam <arindam.chowdhury@infoservices.in>
2018-11-28 14:43:31 +0530

    Capture image email changes

--- src/views/capture-area/capture-area.js
+++ src/views/capture-area/capture-area.js
@@ -1,84 +1,60 @@
-import { withRouter } from "react-router";
-import {
-  compose,
-  lifecycle,
-  pure,
-  withState,
-  withHandlers,
-  withProps
-} from "recompose";
-import viewarApi from "viewar-api";
-import CaptureArea from "./capture-area.jsx";
-import { withDialogControls } from "../../services/dialog";
+import { withRouter } from 'react-router'
+import { compose, lifecycle, pure, withState, withHandlers, withProps } from 'recompose'
+import viewarApi from 'viewar-api'
+import CaptureArea from './capture-area.jsx'
+import { withDialogControls } from '../../services/dialog'
 
-export const goTo = ({ history }) => async route => {
-  history.push(route);
-};
+export const goTo = ({history}) => async (route) => {
+  history.push(route)
+}
 
-export const subjectValue = ({ setSubject }) => async value => {
-  setSubject(value);
-};
+export const subjectValue = ({setSubject}) => async (value) => {
+  setSubject(value)
+}
 
-export const cameraCapture = ({
-  viewarApi,
-  setCurrentScreenshotPath,
-  setCapture
-}) => async () => {
-  await viewarApi.screenshotManager.takeScreenshot();
-  const path = await viewarApi.screenshotManager.saveScreenshot("screenshots");
-  setCurrentScreenshotPath(path);
-  setCapture(false);
-};
+export const cameraCapture =  ({viewarApi, setCurrentScreenshotPath,setCapture}) => async () => {
+  await viewarApi.screenshotManager.takeScreenshot()
+  const path = await viewarApi.screenshotManager.saveScreenshot('screenshots')
+  setCurrentScreenshotPath(path)
+  setCapture(false)
+}
 
-export const saveScreenshot = ({ viewarApi, showDialog }) => async () => {
-  await viewarApi.screenshotManager.saveScreenshotToGallery();
-  showDialog("image saved ", {
-    confirmText: "DialogOK"
-  });
-};
 
-export const emailScreenshot = ({
-  viewarApi,
-  currentScreenshotPath,
-  showDialog,
-  subject
-}) => async () => {
-  //await viewarApi.screenshotManager.emailScreenshot()
+export const saveScreenshot = ({viewarApi,showDialog}) => async () => {
+  await viewarApi.screenshotManager.saveScreenshotToGallery()
+   showDialog("image saved ", {
+        confirmText: 'DialogOK'
+    });  
+}
+
+export const emailScreenshot = ({viewarApi,currentScreenshotPath,showDialog,subject}) => async () => {
+  //await viewarApi.screenshotManager.emailScreenshot()  
   //await viewarApi.appUtils.sendEmail({subject:'Subject',message:'Message',attachments: {currentScreenshotPath} });
   viewarApi.appUtils.sendEmail({
     subject: subject,
-    message: "",
+    message: '',
+    recipients: ['daniel@vonuebermorgen.de'],
     attachments: currentScreenshotPath
   });
-};
+}
 
-export const shareScreenshot = ({
-  viewarApi,
-  currentScreenshotPath
-}) => async () => {
-  const screenshotManager = viewarApi.screenshotManager;
-  await screenshotManager.shareScreenshot("native", currentScreenshotPath);
-};
+export const shareScreenshot = ({viewarApi, currentScreenshotPath}) => async () => {
+  const screenshotManager = viewarApi.screenshotManager
+  await screenshotManager.shareScreenshot('native', currentScreenshotPath)
+}
 
-export const cancelScreenshot = ({
-  viewarApi,
-  setCurrentScreenshotPath,
-  setCapture
-}) => async () => {
-  setCurrentScreenshotPath(null);
-  setCapture(true);
-};
-export const getScreenshotImage = ({
-  viewarApi,
-  currentScreenshotPath
-}) => () => viewarApi.coreInterface.resolveUrl(currentScreenshotPath);
+export const cancelScreenshot = ({viewarApi, setCurrentScreenshotPath,setCapture}) => async () => {
+  setCurrentScreenshotPath(null)
+  setCapture(true)
+}
+export const getScreenshotImage = ({viewarApi, currentScreenshotPath}) => () => viewarApi.coreInterface.resolveUrl(currentScreenshotPath)
 
 export default compose(
   withRouter,
   withDialogControls,
-  withState("currentScreenshotPath", "setCurrentScreenshotPath", null),
-  withState("capture", "setCapture", true),
-  withState("subject", "setSubject", ""),
+  withState('currentScreenshotPath', 'setCurrentScreenshotPath', null),
+  withState('capture', 'setCapture', true),
+  withState('subject', 'setSubject', ''),
   withProps({
     viewarApi
   }),
@@ -93,12 +69,12 @@ export default compose(
     subjectValue
   }),
   lifecycle({
-    async componentDidMount() {
+    async componentDidMount () {
       const camera = viewarApi.cameras.arCamera;
       await camera.activate();
     },
-    async componentWillUnmount() {
-      await viewarApi.cameras.arCamera.unfreeze();
+    async componentWillUnmount () {
+      await viewarApi.cameras.arCamera.unfreeze()
     }
-  })
-)(CaptureArea);
+  }),
+)(CaptureArea)
